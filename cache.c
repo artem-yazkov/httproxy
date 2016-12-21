6c1a8858a989bac736abbcdfb286e0a63293c5f1
Artem <artem.yazkov@gmail.com>
2016-12-22 01:14:04 +0300

    direct mode prototype

--- cache.c
+++ cache.c
@@ -3,6 +3,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+
 #include "cache.h"
 
 /* append binary data to string */
@@ -101,7 +109,41 @@ __parse_field(char *header, char **name, char **value)
 static void
 __get_response(char *host, cache_str_t *req, cache_str_t *resp)
 {
-    fprintf(stdout, "host: %s\n", host);
+    struct hostent *hp;
+    struct sockaddr_in addr;
+    int sock;
+
+    if((hp = gethostbyname(host)) == NULL){
+        herror("gethostbyname");
+        exit(1);
+    }
+    bcopy(hp->h_addr, &addr.sin_addr, hp->h_length);
+    addr.sin_port = htons(80);
+    addr.sin_family = AF_INET;
+    sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    //int on = 1;
+    //setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, (const char *)&on, sizeof(int));
+
+    if(sock == -1){
+        perror("setsockopt");
+        exit(1);
+    }
+    if(connect(sock, (struct sockaddr *)&addr, sizeof(struct sockaddr_in)) == -1){
+        perror("connect");
+        exit(1);
+    }
+
+    char buffer[1024];
+    write(sock, "GET /\r\n", strlen("GET /\r\n")); // write(fd, char[]*, len);..
+    bzero(buffer, sizeof(buffer));
+
+    while(read(sock, buffer, sizeof(buffer) - 1) != 0){
+        fprintf(stderr, "%s", buffer);
+        bzero(buffer, sizeof(buffer));
+    }
+
+    shutdown(sock, SHUT_RDWR);
+    close(sock);
 }
 
 void
@@ -149,10 +191,10 @@ cache_req_process(cache_request_t *req)
         } else {
             __str_append_f(&req->hdr_srv, "%s: %s\n", fname, fvalue);
         }
-        //fprintf(stdout, "\"%s\"  \"%s\"\n", fname, fvalue);
     }
 
-    __get_response(mhost.data, &req->hdr_srv, &req->response);
+    __get_response(mhost.data, &req->hdr_cli, &req->response);
+    //fwrite(req->response.data, req->response.len, 1, stdout);
 
     req->state = REQSTATE_FROM_MEMORY;
     req->cursor = 0;
